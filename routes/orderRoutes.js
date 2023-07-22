9216951e744be10ef5535c052dc879ec992b9c0e
Ginaxd <ada_wit09@hackademy.mx>
2023-07-22 11:25:58 -0600

    registro de tablas para ordenes y consulta paara listar las ordenes

---
+++ routes/orderRoutes.js
@@ -0,0 +1,16 @@
+const OrdersController = require('../controllers/ordersController');
+const passport = require('passport');
+
+module.exports = (app) => {
+
+    // GET -> OBTENER DATOS
+    // POST -> ALMACENAR DATOS
+    // PUT -> ACTUALIZAR DATOS
+    // DELETE -> ELIMINAR DATOS
+
+
+    app.get('/api/orders/findByStatus/:status',  passport.authenticate('jwt', { session: false }), OrdersController.findByStatus);
+
+    app.post('/api/orders/create',  passport.authenticate('jwt', { session: false }), OrdersController.create);
+
+}
